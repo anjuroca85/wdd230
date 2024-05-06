4e984c52ec54201002064e7c3dcb4c180c01baa9
anjuroca85 <anjuroca85@gmail.com>
2024-05-06 17:11:41 -0500

    chamber js removing strong

--- chamber/scripts/getDatesChamber.js
+++ chamber/scripts/getDatesChamber.js
@@ -14,17 +14,8 @@ document.addEventListener("DOMContentLoaded", function () {
   // Getting the current time
   const currentTime = new Date().toLocaleTimeString("en-US", { hour12: false });
 
-  // Creating a strong element for "Last modified:" so that user can see it clearly.
-  const strongLastModified = document.createElement("strong");
-  strongLastModified.textContent = "Last modified: ";
-
-  // Creating a strong element for the formatted last modified date and time for the same above reason
-  const strongDateAndTime = document.createElement("strong");
-  strongDateAndTime.textContent = `${formattedLastModifiedDate}, Time: ${currentTime}`;
-
   // Updating the content of the paragraph element with the ID "lastModified" to display the last modified date and time
-  const lastModifiedParagraph = document.getElementById("lastModified");
-  lastModifiedParagraph.textContent = ""; // Clearing existing content
-  lastModifiedParagraph.appendChild(strongLastModified); // Appending the strong element for "Last modified:"
-  lastModifiedParagraph.appendChild(strongDateAndTime); // Appending the strong element for the formatted date and time
+  document.getElementById(
+    "lastModified"
+  ).textContent = `Last modified: ${formattedLastModifiedDate}, Time: ${currentTime}`;
 });
