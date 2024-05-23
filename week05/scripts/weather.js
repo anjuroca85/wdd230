41bcc8d401036d801942bfe9589ab42e40898dfb
anjuroca85 <anjuroca85@gmail.com>
2024-05-23 10:58:35 -0500

    Week5 Weather API completed

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,30 +1,32 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#current-icon');
-const captionDesc = document.querySelector('figcaption');
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid={b92da245303a816497b90a4eeb90ddf5}&units=imperial'
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=b92da245303a816497b90a4eeb90ddf5&units=imperial";
 
 async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            console.log(data); //testing only. display results data. uncomment when checked.
-        } else {
-            throw Error(await response.text());
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      //console.log(data); //testing only.
+      displayResults(data); //uncomment when checked.
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.____} &deg; F`;
-    const iconsrc = `https://openweathermap.org/img/w/${________}._____`;
-    let desc = data.weather[0].____;
-    weatherIcon.setAttribute('__', ____);
-    weatherIcon.setAttribute('__', ____);
-    captionDesc.textContent = `${desc}`;
+  currentTemp.innerHTML = `${data.main.temp} &deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
 }
